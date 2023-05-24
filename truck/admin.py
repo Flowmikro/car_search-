d41cd3735152f759bddb9b77322cc7288b52f9f3
Flowmikro <cloplop091@gmail.com>
2023-05-24 12:06:48 +0300

    creating a truck model

---
+++ truck/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import TruckModel
+
+admin.site.register(TruckModel)
---
+++ truck/models.py
@@ -0,0 +1,42 @@
+import random
+import string
+from django.db import models
+
+
+class TruckModel(models.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not self.sku:
+            self.sku = self.random_sku()
+            self.location = self.random_location()
+
+    def random_sku(self):
+        num = random.randint(1000, 9999)
+        letter = random.choice(string.ascii_uppercase)
+        return f"{num}{letter}"
+
+    def random_location(self):
+        with open('C:/Users/islam/car_search-/uszips.csv') as f:
+            lines = f.readlines()
+        line = random.choice(lines)
+        zip_code, lat, lng, city, state, *_ = line.split(',')
+        return {
+            'zip_code': zip_code,
+            'city': city,
+            'state': state,
+            'lat': lat,
+            'lng': lng,
+        }
+
+    def save(self, *args, **kwargs):
+        if not self.location:
+            location_data = self.random_location()
+            self.location = f"{location_data['city']}, {location_data['state']}"
+        super().save(*args, **kwargs)
+
+    location = models.CharField(max_length=255, blank=True, null=True)
+    load_capacity = models.PositiveSmallIntegerField()
+    sku = models.CharField(max_length=5, unique=True, blank=True)
+
+
+
